24fa26cfcf68dce9c185f08fd90c78b2f531c66d
susemeeee <hffg123@naver.com>
2020-03-01 01:20:36 +0900

    Update line drawing function

--- src/com/thunder_cut/graphics/feature/LineDrawer.java
+++ src/com/thunder_cut/graphics/feature/LineDrawer.java
@@ -8,12 +8,12 @@ package com.thunder_cut.graphics.feature;
 import com.thunder_cut.graphics.ui.drawing.CanvasPixelInfo;
 
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-public class LineDrawer extends ShapeDrawer{
+public class LineDrawer extends ShapeDrawer {
+    protected double currentX;
+    protected double currentY;
     protected boolean isPlusX;
     protected boolean isPlusY;
     protected int deltaX;
@@ -30,98 +30,57 @@ public class LineDrawer extends ShapeDrawer{
         deltaY = Math.abs(endYPos - startYPos);
         isMaxDeltaX = deltaX > deltaY;
 
-        if((deltaX == 0) || (deltaY == 0)) {
+        if ((deltaX == 0) || (deltaY == 0)) {
             drawNotDiagonalLine(canvasPixelInfo, color);
             return;
         }
 
-        Map<Integer, Integer> ratioMap = new HashMap<>();
-        List<Integer> ratios = new ArrayList<>();
-        makeRatioList(ratioMap, ratios);
+        double ratio = (double) Math.max(deltaX, deltaY) / Math.min(deltaX, deltaY);
+        double count = 0;
+        Map<Double, Double> correctPositions = new HashMap<>();
 
-        currentX = startXPos;
-        currentY = startYPos;
+        while ((currentX != endXPos) && (currentY != endYPos)) {
+            if (isMaxDeltaX) {
+                correctPositions.put(currentX, currentY);
+            } else {
+                correctPositions.put(currentY, currentX);
+            }
 
-        drawLine(ratioMap, ratios, canvasPixelInfo, color);
+            if ((count >= ratio)) {
+                controlPosition(!isMaxDeltaX, isMaxDeltaX ? isPlusY : isPlusX, 1);
+                count = 0;
+            } else {
+                controlPosition(isMaxDeltaX, isMaxDeltaX ? isPlusX : isPlusY, 0.01);
+                count += 0.01;
+            }
+        }
+
+        for (Map.Entry<Double, Double> entry : correctPositions.entrySet()) {
+            if (isMaxDeltaX) {
+                currentX = entry.getKey();
+                currentY = entry.getValue();
+            } else {
+                currentX = entry.getValue();
+                currentY = entry.getKey();
+            }
+            setPixels(canvasPixelInfo, (int) currentX, (int) currentY, color);
+        }
     }
 
     private void drawNotDiagonalLine(CanvasPixelInfo canvasPixelInfo, Color color) {
-        while((currentX != endXPos) || (currentY != endYPos)) {
-            setPixels(canvasPixelInfo, currentX, currentY, color);
+        while ((currentX != endXPos) || (currentY != endYPos)) {
+            setPixels(canvasPixelInfo, (int) currentX, (int) currentY, color);
 
-            controlPosition(isMaxDeltaX, isMaxDeltaX ? isPlusX : isPlusY);
+            controlPosition(isMaxDeltaX, isMaxDeltaX ? isPlusX : isPlusY, 1);
         }
     }
 
-    protected void makeRatioList(Map<Integer, Integer> ratioMap, List<Integer> ratios) {
-        int pixelMoveRatio = Math.max(deltaX, deltaY) / Math.min(deltaX, deltaY);
-        int count = 0;
-
-        while((currentX != endXPos) && (currentY != endYPos)) {
-            if(!ratioMap.containsKey(pixelMoveRatio)) {
-                ratioMap.put(pixelMoveRatio, 1);
-                ratios.add(pixelMoveRatio);
-            }
-            else {
-                ratioMap.replace(pixelMoveRatio, ratioMap.get(pixelMoveRatio) + 1);
-            }
-
-            if((count >= pixelMoveRatio)) {
-                controlPosition(!isMaxDeltaX, isMaxDeltaX ? isPlusY : isPlusX);
-                count = 0;
-
-                deltaX = Math.abs(endXPos - currentX);
-                deltaY = Math.abs(endYPos - currentY);
-                pixelMoveRatio =  Math.max(deltaX, deltaY) / Math.min(deltaX, deltaY);
-            }
-            else {
-                controlPosition(isMaxDeltaX, isMaxDeltaX ? isPlusX : isPlusY);
-                count++;
-            }
+    protected void controlPosition(boolean isMaxDeltaX, boolean isPlus, double moveDistance) {
+        if (isMaxDeltaX) {
+            currentX += isPlus ? moveDistance : moveDistance * -1;
+            return;
         }
-    }
 
-    protected void drawLine(Map<Integer,Integer> ratioMap, List<Integer> ratios,
-                          CanvasPixelInfo canvasPixelInfo, Color color) {
-        int mapRatio;
-        int count = 0;
-
-        int pixelMoveRatio;
-        if(ratios.size() == 1) {
-            mapRatio = 0;
-            pixelMoveRatio = ratioMap.get(ratios.get(0));
-        }
-        else {
-            mapRatio = Math.max(ratioMap.get(ratios.get(0)), ratioMap.get(ratios.get(1))) /
-                    Math.min(ratioMap.get(ratios.get(0)), ratioMap.get(ratios.get(1)));
-            pixelMoveRatio = (Math.max(ratioMap.get(ratios.get(0)), ratioMap.get(ratios.get(1))) ==
-                    ratioMap.get(ratios.get(0))) ? ratios.get(0) : ratios.get(1);
-        }
-        int ratioCount = 0;
-
-        while((currentX != endXPos) && (currentY != endYPos)) {
-            setPixels(canvasPixelInfo, currentX, currentY, color);
-
-            if((count >= pixelMoveRatio)) {
-                controlPosition(!isMaxDeltaX, isMaxDeltaX ? isPlusY : isPlusX);
-                count = 0;
-                ratioCount++;
-
-                if(ratios.size() == 1) {
-                    continue;
-                }
-                if((ratioCount >= mapRatio)) {
-                    pixelMoveRatio = (pixelMoveRatio == ratios.get(0)) ? ratios.get(1) : ratios.get(0);
-                    ratioCount = 0;
-                }
-                else if(pixelMoveRatio != Math.max(ratioMap.get(ratios.get(0)), ratioMap.get(ratios.get(1)))) {
-                    pixelMoveRatio = (pixelMoveRatio == ratios.get(0)) ? ratios.get(1) : ratios.get(0);
-                }
-            }
-            else {
-                controlPosition(isMaxDeltaX, isMaxDeltaX ? isPlusX : isPlusY);
-                count++;
-            }
-        }
+        currentY += isPlus ? moveDistance : moveDistance * -1;
     }
 }
